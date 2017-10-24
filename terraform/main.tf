be1fdcb38037647d4d8e2b7c5cbbded41840bf49
Ubuntu <naoko.vp@gmail.com>
2017-10-24 18:36:26 +0000

    Add features + Fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,25 @@
-variable "nsg" {
-  default = "security-group-webapp"
+# DECLARATION S3
+terraform {
+  backend "s3" {
+    bucket = "backend.terraform"
+    key    = "webapp/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "infrastructure" {
+  backend = "s3"
+
+  config {
+    bucket = "backend.terraform"
+    key    = "vpc/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+# TEMPLATE USERDATA
+data "template_file" "user_data" {
+  template = "${file("userdata.tpl")}"
 }
 
 # PROVIDER
@@ -7,26 +27,27 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-# WEBAPP
+# INSTANCE
 resource "aws_instance" "webapp" {
+  count                       = "2"
   ami                         = "ami-785db401"
   instance_type               = "t2.micro"
   key_name                    = "Valentin"
   associate_public_ip_address = "true"
-  subnet_id                   = "subnet-0648dd5d"
+  subnet_id                   = "${element(data.terraform_remote_state.infrastructure.subnet_all, count.index)}"
   security_groups             = ["${aws_security_group.security-group-webapp.id}"]
+  user_data                   = "${data.template_file.user_data.rendered}"
 
   tags {
-    Name = "WEBAPP-HelloWorld"
+    Name = "WEBAPP-${count.index}"
   }
 }
 
 # SECU GROUP
-
 resource "aws_security_group" "security-group-webapp" {
   name        = "security-group-webapp"
   description = "Filtrage"
-  vpc_id      = "vpc-37f8a750"
+  vpc_id      = "${data.terraform_remote_state.infrastructure.vpc_id}"
 
   ingress {
     from_port   = 22
@@ -52,4 +73,28 @@ resource "aws_security_group" "security-group-webapp" {
   tags {
     Name = "WEBAPP-SecuGroup"
   }
+}
+
+# CREATION ELB
+resource "aws_elb" "elb" {
+  name            = "elb-infra"
+  subnets         = ["${data.terraform_remote_state.infrastructure.subnet_all}"]
+  security_groups = ["${aws_security_group.security-group-webapp.id}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 2
+    target              = "HTTP:80/"
+    interval            = 5
+  }
+
+  instances = ["${aws_instance.webapp.*.id}"]
 }
